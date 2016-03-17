653d6adafa4b329885353f8996966ad4358abaf5
KennethWilke <kenneth.wilke@rackspace.com>
2016-03-17 08:33:59 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="Packilog",
-    version="0.0.2",
+    version="0.0.3",
     author="Kenneth Wilke",
     author_email="kenneth.wilke@rackspace.com",
     packages=['libpackilog', 'libpackilog.test'],
